aa01a8cd43237abdd56c79f4a44a6c2a530eadb3
Nate Perfetti <nathanperfetti@gmail.com>
2018-11-04 20:07:02 -0800

    Committed a happy-lil on-resize event to clear rainbow grid

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,9 +1,7 @@
 const canvas = document.querySelector('canvas');
 
 const c = canvas.getContext('2d')
-canvas.width = window.innerWidth;
 
-canvas.height = window.innerHeight;
 
 const debounce = (func) => {
     let timer
@@ -11,12 +9,22 @@ const debounce = (func) => {
       if (timer) { clearTimeout(timer) }
       timer = setTimeout(func, 100, event)
     }
-  }
+  };
   
   window.addEventListener('resize', debounce(() => {
     canvas.width = window.innerWidth
     canvas.height = window.innerHeight
-  }))
+  }));
 
+  
+  c.fillStyle = 'white';
+  c.fillRect(100, 100, 100, 100);
 
-  c.fillRect(100, 100, 100, 100)
+  for (let i = 0; i < 60; i++) {
+    for (let j = 0; j < 60; j++) {
+      c.fillStyle = `rgb(${i * 5}, ${j * 5}, ${(i+j) * 50})`
+      c.fillRect(j * 20, i * 20, 10, 10)
+    }
+  }
+
+  console.log("Were in!!!")
